40eb7457743acc2debd50c6f18d16af8e5f6542a
Sowndharayaa <71545646+Sowndharayaa@users.noreply.github.com>
2021-04-29 16:28:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database = firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+  var Pos = database.ref('ball/position')
+  Pos.on("value",readposition,error)
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    updateHeight(0,-10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale -0.01
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+function updateHeight(x,y){
+  database.ref('balloon/height').set({
+    'x':height.x + x,
+    'y':height.y+y
+  })
+}
+function readposition(data){
+  position = data.val()
+  console.log(position.x);
+  ball.x = position.x;
+  ball.y = position.y;
+ }
+ function error(){
+     console.log("Server is down, try again later");
+ }
